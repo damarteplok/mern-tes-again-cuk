4e9f7d6b7cae33179f22c63ee0106b195aac910b
damar huda <damarhuda@live.com>
2021-06-01 20:15:03 +0700

    adadasdas

---
+++ data.js
@@ -0,0 +1,13 @@
+
+
+
+export const data = [
+    {
+        title: 'Hello World',
+        subtitle: 'Hello World sub'
+    },
+    {
+        title: 'Hello World2',
+        subtitle: 'Hello World sub 2'
+    }
+]
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,9 +1,10 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
-import axios from "axios";
-import Link from "next/link";
 import BasePage from "@/components/BasePage";
+import Link from "next/link";
+import {useGetPosts} from "@/actions";
 
-const Portfolios = ({posts}) => {
+const Portfolios = () => {
+    const {posts, error} = useGetPosts()
 
     const renderPosts = () => {
         return posts.map(post =>
@@ -21,24 +22,32 @@ const Portfolios = ({posts}) => {
         <BaseLayout>
             <BasePage>
                 <h1>I am portfolios page</h1>
-                <ul>
-                    {renderPosts()}
-                </ul>
+                {   posts &&
+                        <ul>
+                            {renderPosts()}
+                        </ul>
+                }
+                {
+                    error &&
+                        <div className="alert alert-danger">{error.message}</div>
+                }
+
             </BasePage>
         </BaseLayout>
     )
 }
 
-Portfolios.getInitialProps = async () => {
-    let posts = [];
-    try {
-        const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
-        posts = res.data
-    } catch (e) {
-        console.error(e)
-    }
-
-    return {posts: posts.slice(0, 10)}
-}
+// buat init props dari next
+// Portfolios.getInitialProps = async () => {
+//     let posts = [];
+//     try {
+//         const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+//         posts = res.data
+//     } catch (e) {
+//         console.error(e)
+//     }
+//
+//     return {posts: posts.slice(0, 10)}
+// }
 
 export default Portfolios
